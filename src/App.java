e0fef3d40c145f9de15697f91dadfd4a590ee43e
Alcides Bueno jr <alcides.junior@ambevtech.com.br>
2022-07-20 20:17:10 -0300

    Extraindo dados

--- src/App.java
+++ src/App.java
@@ -2,22 +2,25 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
-import java.net.http.HttpResponse.BodyHandler;
 import java.net.http.HttpResponse.BodyHandlers;
+import java.util.List;
+import java.util.Map;
 
 public class App {
     public static void main(String[] args) throws Exception {
 
-        String url = "https://imdb-api.com/en/API/Top250Movies/k_pr4z1xy6";
+        String url = "https://alura-filmes.herokuapp.com/conteudos";
         // conectar com a API
         URI endereco = URI.create(url);
         var client = HttpClient.newHttpClient();
         var request = HttpRequest.newBuilder(endereco).GET().build();
         HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
         String body = response.body();
-        System.out.println(body);
 
         // extrair só os dados(Titulo, poster, calssificação)
+        var parser = new JsonParser();
+        List<Map<String, String>> listaDeFilmes = parser.parse(body);
+        System.out.println(listaDeFilmes.size());
 
     }
 }
