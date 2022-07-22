2d489e5230a90bd01bfd4cc322fa79cf80475ade
Alcides Bueno jr <alcides.junior@ambevtech.com.br>
2022-07-22 16:37:06 -0300

    Ajustes imagem

--- src/StickerGenerator.java
+++ src/StickerGenerator.java
@@ -1,7 +1,10 @@
 import java.io.File;
 import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
+import java.awt.Color;
+import java.awt.Font;
 import javax.imageio.ImageIO;
+import javax.swing.text.AttributeSet.ColorAttribute;
 
 public class StickerGenerator {
 
@@ -22,7 +25,13 @@ public class StickerGenerator {
     Graphics2D graphics = (Graphics2D) novaImagem.getGraphics();
     graphics.drawImage(imagemOriginal, 0, 0, null);
 
+    // configurar a fonte
+    var fonte = new Font(Font.SANS_SERIF, Font.BOLD, 64);
+    graphics.setColor(Color.YELLOW);
+    graphics.setFont(fonte);
+
     // escrever uma frase na nova imagem
+    graphics.drawString("TOPZERA", 0, novaAltura - 100);
 
     // escrever a nova imagem em um arquivo
 
